06ca7b5de01490271dc751ce5119ce1ae6ba559c
Lubna <muspaksamsu@gmail.com>
2024-07-01 22:42:38 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+// Guest List: If you could invite anyone, living or deceased, to dinner, 
+//who would you invite? Make a list that includes at least three people you’d like to invite to dinner. 
+//Then use your list to print a message to each person, inviting them to dinner.
+var Guest_List = ["Lubna", "Fatima", "Shahina", "aqsa"];
+for (var i = 0; i < Guest_List.length; i++) {
+    console.log("dear ".concat(Guest_List[i], " : \n you are invited to dinner! \n1"));
+}
---
+++ main.ts
@@ -0,0 +1,9 @@
+// Guest List: If you could invite anyone, living or deceased, to dinner, 
+//who would you invite? Make a list that includes at least three people you’d like to invite to dinner. 
+//Then use your list to print a message to each person, inviting them to dinner.
+
+let Guest_List : string[] = ["Lubna","Fatima","Shahina","aqsa"];
+for (let i=0;i< Guest_List.length;i++){
+    console.log(`dear ${Guest_List[i]} : \n you are invited to dinner! \n`);
+}
+
